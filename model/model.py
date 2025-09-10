2df1900a55db7b6d948114f5dd4de8481c57904f
111B15308 <145731748+111B15308@users.noreply.github.com>
2025-09-10 22:49:59 +0800

    Add files via upload

---
+++ model/model.py
@@ -0,0 +1,40 @@
+from PyQt5.QtCore import QObject, pyqtSignal
+
+class MapModel(QObject):
+    state_changed = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self._center = {"lat": 25.033964, "lng": 121.564468}  # 預設台北101
+        self._zoom = 14
+        self._markers = []
+
+    @property
+    def center(self):
+        return self._center
+
+    @center.setter
+    def center(self, val):
+        self._center = val
+        self.state_changed.emit()
+
+    @property
+    def zoom(self):
+        return self._zoom
+
+    @zoom.setter
+    def zoom(self, val):
+        self._zoom = val
+        self.state_changed.emit()
+
+    @property
+    def markers(self):
+        return list(self._markers)
+
+    def add_marker(self, marker):
+        self._markers.append(marker)
+        self.state_changed.emit()
+
+    def clear_markers(self):
+        self._markers = []
+        self.state_changed.emit()
